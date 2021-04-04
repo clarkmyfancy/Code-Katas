98e7046ac9d11d62745430f20b13dd01bd77b9c1
Jack Conrad Clark <clark.jack0@gmail.com>
2021-04-04 15:47:42 -0500

    remove unused code

--- Supermarket-Pricing/src/IDate.hs
+++
@@ -1,8 +0,0 @@
-module IDate where
-
-import Data.Time.Clock
-import Data.Time.Calendar
-
-date :: IO(Integer, Int, Int) -- :: (year,month,day)
-date = getCurrentTime >>= return . toGregorian . utctDay
-
