ed9fd7137bff2587339c8492b57e429c7cc0fc5b
Jack Conrad Clark <clark.jack0@gmail.com>
2021-04-04 16:47:11 -0500

    add examples of getting input from user and outputting to console

--- Supermarket-Pricing/src/Console.hs
+++ Supermarket-Pricing/src/Console.hs
@@ -8,6 +8,21 @@ printNBlankLines n = do
     putStr "\n"
     printNBlankLines (n - 1)
 
-showThing :: IO()
-showThing = do
+displayDate :: IO()
+displayDate = do
     Date.date
+
+greetCustomer :: IO()
+greetCustomer = do
+    putStr "Hey! Let's scan some items!"
+
+-- prompt:
+-- showUpperBoundPrompt :: IO() 
+-- showUpperBoundPrompt = putStr "what is the upper bound?  -> "
+
+-- getinputfromuser:
+-- getUpperBoundAndDeriveResults :: IO()
+-- getUpperBoundAndDeriveResults = do 
+--     upperBound <- getLine
+--     printNBlankLines 1
+--     printTranslation (read upperBound :: Int)
