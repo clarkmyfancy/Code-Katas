b5d37992923f68227f72b7d58b72780f71013e18
Jack Conrad Clark <clark.jack0@gmail.com>
2021-04-16 22:02:32 -0500

    update interface with #scanNewItem

--- Supermarket-Pricing/src/Register.hs
+++ Supermarket-Pricing/src/Register.hs
@@ -2,3 +2,7 @@ module Register where
 
 import ConcreteRegister
 
+scanNewItem :: Double -> Double -> Double
+scanNewItem previousCartTotal price = do
+    ConcreteRegister.scanNewItem previousCartTotal price
+
