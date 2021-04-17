faf5cae0fa82111ba9bf99d421a5576a2e59f09e
Jack Conrad Clark <clark.jack0@gmail.com>
2021-04-16 21:50:37 -0500

    add feature to keep a running total

--- Supermarket-Pricing/src/Console.hs
+++ Supermarket-Pricing/src/Console.hs
@@ -17,26 +17,22 @@ greetCustomer = do
     putStr "Hey! Let's scan some items!"
     printNBlankLines 1 
 
-continuouslyPromptUserForItems :: IO()
-continuouslyPromptUserForItems = do    
-    item <- getLine
-    putStr item
-    if item == (show 0)
+continuouslyPromptUserForItems :: Double -> IO()
+continuouslyPromptUserForItems previousCartTotal = do  
+    userInput <- getLine
+    let price = convertToDouble userInput
+    let newTotal = scanNewItem price previousCartTotal
+    putStr ("Total: " ++ (show newTotal))
+    if userInput == (show 0)
         then do 
             printNBlankLines 1
         else do 
             printNBlankLines 2
-            continuouslyPromptUserForItems
+            continuouslyPromptUserForItems newTotal
 
-    
+convertToDouble :: [Char] -> Double
+convertToDouble x = read x :: Double
 
--- prompt:
--- showUpperBoundPrompt :: IO() 
--- showUpperBoundPrompt = putStr "what is the upper bound?  -> "
-
--- getinputfromuser:
--- getUpperBoundAndDeriveResults :: IO()
--- getUpperBoundAndDeriveResults = do 
---     upperBound <- getLine
---     printNBlankLines 1
---     printTranslation (read upperBound :: Int)
+scanNewItem :: Double -> Double -> Double
+scanNewItem previousCartTotal price = do 
+    previousCartTotal + price
